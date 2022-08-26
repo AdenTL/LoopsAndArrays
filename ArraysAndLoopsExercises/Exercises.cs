c76fc20ab7bc7242e651b368dd7fae29f29b9f45
AdenTL <15adenlangdon@gmail.com>
2022-08-26 18:26:44 -0400

    Repo Origin Reset

--- ArraysAndLoopsExercises/Exercises.cs
+++ ArraysAndLoopsExercises/Exercises.cs
@@ -17,6 +17,17 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("SORT THE NUMBERS\n");
 
             // Enter your solution here
+            int[] favoriteNumbers = new int[10];
+            for (int i = 0; i < favoriteNumbers.Length; i++)
+            {
+                Console.WriteLine("Please enter your favorite number: ");
+                favoriteNumbers[i] = Int32.Parse(Console.ReadLine());
+            }
+            Array.Sort(favoriteNumbers);
+            Console.WriteLine(favoriteNumbers[0]);
+            Console.WriteLine(favoriteNumbers[favoriteNumbers.Length - 1]);
+            
+         
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
@@ -34,6 +45,18 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("WHICH DAY OF THE WEEK?\n");
 
             // Enter your solution here
+            string[] daysOfWeek = new string[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+            for(int i = 0; i < daysOfWeek.Length; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+            while (true)
+            {
+                Console.WriteLine("Please enter a number 1 - 7");
+                int userInput = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine(daysOfWeek[userInput - 1]);
+            }
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
@@ -48,7 +71,14 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("FIND THE SMALLEST NUMBER IN AN ARRAY\n");
 
             // Enter your solution here
-
+            int[] arrInt = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter an integer: ");
+                arrInt[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The smallest number is: " + arrInt.Min());
+  
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
@@ -62,6 +92,8 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("SHOPPING LIST\n");
 
             // Enter your solution here
+            string[] arrFood = new string[] {"Onions", "Tomatos", "Lettuce", "Pickles"};
+            Array.ForEach(arrFood, Console.WriteLine);   //this is COOL
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
@@ -76,7 +108,26 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("WHAT'S THE LONGEST WORD?\n");
 
             // Enter your solution here
-
+            Console.WriteLine("Type in a phrase thats one to four words long (no punctuation)");
+            string phrase = Console.ReadLine();
+            string[] words = phrase.Split(' ');
+            string longestWordTwo = "";
+            string longestWord = "";
+            int longestLength = 0;
+            for(int i = 0; i < words.Length; i++)
+            {
+                int length = words[i].Length;
+                if (length > longestLength)
+                {
+                    longestWord = words[i];      
+                    longestLength = length;
+                }
+                else if (length == longestLength)
+                {
+                    longestWordTwo = words[i];
+                }
+            }
+            Console.WriteLine(longestWord + ' ' + longestWordTwo);
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
@@ -90,7 +141,15 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("WHAT'S THE SECOND TO LAST WORD?\n");
 
             // Enter your solution here
-
+            Console.WriteLine("Type in a sentence with at least four words.");
+            string phrase = Console.ReadLine();
+            string[] words = phrase.Split(' ');
+            string secondToLast = "";
+            for(int i = 0; i < words.Length; i++)
+            {
+                secondToLast = words[i];               //this turns secondToLast into whatever word the loop is at. i need it to print the 
+            }                                          //second to last index. i - 2 dosent work that way... thatd be too easy
+            Console.WriteLine(words);
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
@@ -119,6 +178,20 @@ namespace ArraysAndLoopsExercises
             Console.WriteLine("COMMON VALUES\n");
 
             // Enter your solution here
+            int commonValue = 0;
+            int[] arrayOne = new int[] {1, 2, 3, 4, 5};
+            int[] arrayTwo = new int[] {3, 6, 7, 2, 1};
+            for(int i = 0; i < arrayTwo.Length; i++)
+            {
+                for (int j = 0; j < arrayOne.Length; j++)                   // dosent update commonValue anywhere
+                {
+                    if (arrayOne[j] == arrayTwo[i])
+                    {
+                        commonValue = 0;
+                    }
+                }
+            }
+            Console.WriteLine(commonValue);
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
